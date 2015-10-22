fd9f69d7bdf8eadfb852e7d024c78dbff8e996cc
unknown <Yahor_Faliazhynski@EPBYMINW1799T1.minsk.epam.com>
2015-10-22 15:41:02 +0300

    Second commit

---
+++ src/main/java/com/github/rogerzorge/helloci_for_training/App.java
@@ -0,0 +1,17 @@
+package com.github.rogerzorge.helloci_for_training;
+
+/**
+ * Hello CI!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+    
+    public static void doNothing() {
+    	// do nothing
+    }
+}
---
+++ src/main/java/com/github/rogerzorge/helloci_for_training/EmptyClass.java
@@ -0,0 +1,18 @@
+package com.github.rogerzorge.helloci_for_training;
+
+/**
+ * Empty class for Cobertura code coverage
+ * @author vitali_shulha
+ *
+ */
+public class EmptyClass {
+	
+	public static void emptyMethod() {
+		// empty
+	}
+	
+	public static void unusedMethod() {
+		// do not use it
+	}
+
+}
